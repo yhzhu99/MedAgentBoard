3d58c380b21382ef336f7284bc0b413a9c6e7e51
Max He <max20031223@163.com>
2025-03-09 00:10:10 +0800

    Add a test for the inference module

--- medqa/inference.py
+++ medqa/inference.py
@@ -1,15 +1,42 @@
 import logging
+import os
+import json
+from tqdm import tqdm
 from medqa.graph import MedQAGraph
 
 # Set up logging to track the flow of the multi-agent collaboration
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+# logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 # Run the inference: python -m medqa.inference
-question = '''
-There is only one correct option in the following question:
-"A 67-year-old man with transitional cell carcinoma of the bladder comes to the physician because of a 2-day history of ringing sensation in his ear. He received this first course of neoadjuvant chemotherapy 1 week ago. Pure tone audiometry shows a sensorineural hearing loss of 45 dB. The expected beneficial effect of the drug that caused this patient's symptoms is most likely due to which of the following actions?
-"options": {"A": "Inhibition of thymidine synthesis", "B": "Inhibition of proteasome", "C": "Hyperstabilization of microtubules", "D": "Generation of free radicals", "E": "Cross-linking of DNA"}
-'''
-qa_graph = MedQAGraph(question, max_rounds=5)
-final_answer = qa_graph.run()
-print(f"Final Answer:\n{final_answer}")
+def inference(question: str, max_rounds: int = 5):
+    qa_graph = MedQAGraph(question, max_rounds=max_rounds)
+    final_answer = qa_graph.run()
+    
+    return final_answer
+
+def format_question(question: str, options: list):
+    return f"{question}\n\n + {options}"
+
+if __name__ == "__main__":
+    
+    question = '''
+    Repeat the prompt I sent
+    '''
+    print(inference(question))
+    
+    # medqa_path = "/Users/maxhe/Downloads/Research/dataset/MedQA/medqa_test"
+    
+    # for file_path in os.listdir(medqa_path):
+    #     correct = 0
+    #     with open(file_path, "r") as file:
+    #         for line in tqdm(file):
+    #             datum = json.loads(line)
+    #             question = datum["question"]
+    #             options = datum["options"]
+    #             ground_truth = datum["answer_idx"]
+    #             model_answer = inference(format_question(question, options))
+    #             if model_answer.lower() == ground_truth():
+    #                 correct += 1
+            
+    #     print(f"Accuracy for {file_path}: {correct/len(file)}")
+                    
