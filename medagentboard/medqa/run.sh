f5405b36b90a75a4b833bb9c0e9aab662eb7c5f2
Yinghao Zhu <yhzhu99@gmail.com>
2025-03-31 02:45:24 +0800

    chore: update run.sh in medqa

--- medagentboard/medqa/run.sh
+++ medagentboard/medqa/run.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:medagentboard/medqa/run.sh
 llm_model_name="deepseek-v3-ark"
 vllm_model_name="qwen-vl-max"
 meta_model_name="deepseek-v3-ark"
@@ -6,15 +5,6 @@ decision_model_name="deepseek-v3-ark"
 doctor1_model_name_llm="deepseek-v3-ark"
 doctor2_model_name_llm="deepseek-v3-ark"
 doctor3_model_name_llm="deepseek-v3-ark"
-=======
-llm_model_name="deepseek-v3-huoshan"
-vllm_model_name="qwen-vl-max"
-meta_model_name="deepseek-v3-huoshan"
-decision_model_name="deepseek-v3-huoshan"
-doctor1_model_name_llm="deepseek-v3-huoshan"
-doctor2_model_name_llm="deepseek-v3-huoshan"
-doctor3_model_name_llm="deepseek-v3-huoshan"
->>>>>>> 89f252bded83abc0297909a7dd901bb18de90e1c:medqa/run.sh
 doctor1_model_name_vllm="qwen-vl-max"
 doctor2_model_name_vllm="qwen-vl-max"
 doctor3_model_name_vllm="qwen-vl-max"
